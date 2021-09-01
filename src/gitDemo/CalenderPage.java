88cca4fc2decec1d49ac41e6472d069fe77bff6f
prafpawar11@gmail.com <prafpawar11@gmail.com>
2021-09-01 09:20:51 +0530

    added new deals page and celender page functionality by using cmd mode

---
+++ src/gitDemo/CalenderPage.java
@@ -0,0 +1,9 @@
+package gitDemo;
+
+public class CalenderPage {
+
+	
+	public static void main(String[] args) {
+		System.out.println("calender page");
+	}
+}
---
+++ src/gitDemo/Dealspage.java
@@ -0,0 +1,8 @@
+package gitDemo;
+
+public class Dealspage {
+
+	public static void main(String[] args) {
+		System.out.println("delas page");
+	}
+}
